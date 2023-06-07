dd1a40d975acaf42ebda97e71d037ebd2de3f0da
LeeHW-Git-new <71817406+LeeHW-Git-new@users.noreply.github.com>
2023-06-08 02:49:55 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int solution(int n) 
+{
+    int answer = 0;
+    
+      if(sqrt(n)==(int)sqrt(n)) 
+          answer = 1;
+    else answer = 2;
+    
+    return answer;
+}
