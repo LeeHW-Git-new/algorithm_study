7257b0f8828e55a6c312197377397609d128f2c0
LeeHW-Git-new <71817406+LeeHW-Git-new@users.noreply.github.com>
2025-02-14 11:22:37 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.47 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list) 
+    {
+        Array.Reverse(num_list); // 배열을 뒤집음
+        return num_list; // 뒤집힌 배열 반환
+    }
+}
