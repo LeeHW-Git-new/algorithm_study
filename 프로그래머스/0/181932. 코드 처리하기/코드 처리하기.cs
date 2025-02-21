0f36318164346a2425eb79ec208c18754393c688
LeeHW-Git-new <71817406+LeeHW-Git-new@users.noreply.github.com>
2025-02-21 13:14:58 +0900

    [level 0] Title: 코드 처리하기, Time: 0.78 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+public class Solution 
+{
+    public string solution(string code) 
+    {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+        
+        for (int idx = 0; idx < code.Length; idx++) 
+        {
+            if (code[idx] == '1') 
+            {
+                mode = 1 - mode; // mode 변경
+            } 
+            else 
+            {
+                if ((mode == 0 && idx % 2 == 0) || (mode == 1 && idx % 2 != 0)) 
+                {
+                    ret.Append(code[idx]);
+                }
+            }
+        }
+        return ret.Length > 0 ? ret.ToString() : "EMPTY";
+    }
+}
